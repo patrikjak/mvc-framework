87a2d2d9da6d67e08b61434a9cd1be6c5f56855f
Patrik Jakab <patrikjak@gmail.com>
2021-02-03 14:37:20 +0100

    added routing

---
+++ core/Router.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    public Request $request;
+    protected array $routes = [];
+
+    /**
+     * Router constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false) {
+            echo "Not found :(";
+            die();
+        }
+        echo call_user_func($callback);
+    }
+}
